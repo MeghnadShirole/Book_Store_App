6a5047af5bd8e11b3d6216c279378b80f1e406ef
MeghnadShirole <shirolemeghnad10@gmail.com>
2022-01-26 16:36:10 +0530

    [Meghnad]_Added: validations for login and reset password

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
-import { newUserValidator } from '../validators/user.validator';
+import { newUserValidator, resetPasswordValidator, loginValidator } from '../validators/user.validator';
 import * as role from '../middlewares/role.middleware';
 import { resetPasswordAuth } from '../middlewares/auth.middleware';
 
@@ -13,16 +13,16 @@ router.post('/adminRegistration', newUserValidator, role.setAdmin, userControlle
 router.post('/userRegistration', newUserValidator, role.setUser, userController.newUser);
 
 //route for admin login
-router.post('/adminLogin', role.checkAdmin, userController.login);
+router.post('/adminLogin', loginValidator, role.checkAdmin, userController.login);
 
 //route for user login
-router.post('/userLogin', role.checkUser, userController.login);
+router.post('/userLogin', loginValidator, role.checkUser, userController.login);
 
 //route for forget password
 router.post('/forgetPassword', userController.forgetPassword);
 
 //route for reset password
-router.post('/resetPassword/:token', resetPasswordAuth, userController.resetPassword);
+router.post('/resetPassword/:token', resetPasswordAuth, resetPasswordValidator, userController.resetPassword);
 
 
 export default router;
--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -15,12 +15,49 @@ export const newUserValidator = (req, res, next) => {
             .required(),
 
         password: Joi.string()
+            .min(3)
             .required(),
 
         role: Joi.string(),
     });
 
     const { error, value } = schema.validate(req.body);
+    if (error) {
+        next(error);
+    } else {
+        req.validatedBody = value;
+        next();
+    }
+};
+
+export const resetPasswordValidator = (req, res, next) => {
+    const password = Joi.object({
+        password: Joi.string()
+            .min(3)
+            .required(),
+    });
+
+    const { error, value } = password.validate(req.body);
+    if (error) {
+        next(error);
+    } else {
+        req.validatedBody = value;
+        next();
+    }
+};
+
+export const loginValidator = (req, res, next) => {
+    const password = Joi.object({
+        email: Joi.string()
+            .email()
+            .required(),
+
+        password: Joi.string()
+            .min(3)
+            .required(),
+    });
+
+    const { error, value } = password.validate(req.body);
     if (error) {
         next(error);
     } else {
